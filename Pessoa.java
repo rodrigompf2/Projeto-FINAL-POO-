64f85c2039fd6f79114a317d39527937e5c36d9e
Adryan Lima <adryanlimarj@gmail.com>
2025-09-17 15:39:57 -0300

    classes

---
+++ Pessoa.java
@@ -0,0 +1,42 @@
+package classes;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+
+	private String nome;
+	private String cpf;
+	private LocalDate dataNascimento;
+
+	public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
