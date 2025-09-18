99414ece4c6d9c1aa3873c52464440693a0adb49
MatheusSchwhartz <matheusschwhartz@gmail.com>
2025-09-17 21:53:12 -0300

    Atualizacao

--- src/classes/Dependente.java
+++ src/classes/Dependente.java
@@ -1,18 +1,26 @@
 package classes;
 
+import excecao.DependenteException;
 import java.time.LocalDate;
+import java.time.Period;
 
 import enums.Parentesco;
 
-public class Dependente extends Pessoa { 
-    
-    private Parentesco parentesco;
+public class Dependente extends Pessoa {
+	private Parentesco parentesco;
 
-    public Dependente(String nome, String cpf, LocalDate dataNascimento, Parentesco parentesco) {
-        super(nome, cpf, dataNascimento);
-        this.parentesco = parentesco;
-    }
-
-    public Parentesco getParentesco() { return parentesco; }
-    public void setParentesco(Parentesco parentesco) { this.parentesco = parentesco; }
+	public Dependente(String nome, String cpf, LocalDate dataNascimento, Parentesco parentesco) throws DependenteException {
+		super(nome, cpf, dataNascimento);
+		if (Period.between(dataNascimento, LocalDate.now()).getYears() >=18) {
+			throw new DependenteException("Dependente deve ter menos de 18 anos.");
+		}
+		this.parentesco = parentesco;
+	}
+	
+	
+	public Parentesco getParentesco() {
+		return parentesco;
+	}
+	
+	
 }
--- src/exceptions/DependenteException.java
+++ src/exceptions/DependenteException.java
@@ -1,6 +1,12 @@
 package exceptions;
 
-public class DependenteException extends Exception{
-
+public class DependenteException extends Exception {
 	
+	private static final long serialVersionUID = 1L;
+
+	public DependenteException(String mensagem) {
+		super(mensagem);
+		
+		
+	}
 }
