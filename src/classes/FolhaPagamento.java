28837e406764d9d9fe688064f178545a0b284e18
AdryanDev7 <adryanlimarj@gmail.com>
2025-09-19 18:03:44 -0300

    alteração

--- src/classes/FolhaPagamento.java
+++ src/classes/FolhaPagamento.java
@@ -1,12 +1,12 @@
 package classes;
-
 import java.time.LocalDate;
-
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class FolhaPagamento {
 
     private int codigo;
-    private Funcionario funcionario; 
+    private Funcionario funcionario;
     private LocalDate dataPagamento;
     private double descontoINSS;
     private double descontoIR;
@@ -22,7 +22,26 @@ public class FolhaPagamento {
         this.salarioLiquido = salarioLiquido;
     }
 
-    public int getCodigo() {
+    @Override
+    public String toString() {
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        		return  "============================\n"  +
+        				"		Folha Pagamento		 \n"  +
+        				"============================\n"  +
+                        "Código: " + this.codigo +  "\n"  +
+                        "Data de Pagamento: " + this.dataPagamento.format(formatador) + "\n" +
+                        "Funcionário: " + this.funcionario.getNome() + " (CPF: " + this.funcionario.getCpf() + ")\n" +
+                        "---------------------------------------\n" +
+                        String.format(Locale.forLanguageTag("pt-BR"), "Salário Bruto:   R$ %.2f\n", this.funcionario.getSalarioBruto()) +
+                        String.format(Locale.forLanguageTag("pt-BR"), "(-) Desconto INSS: R$ %.2f\n", this.descontoINSS) +
+                        String.format(Locale.forLanguageTag("pt-BR"), "(-) Desconto IR:   R$ %.2f\n", this.descontoIR) +
+                        "---------------------------------------\n" +
+                        String.format(Locale.forLanguageTag("pt-BR"), "Salário Líquido: R$ %.2f\n", this.salarioLiquido) +
+                        "=======================================\n";
+    }
+
+	public int getCodigo() {
         return codigo;
     }
 
